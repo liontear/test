4ab4448354e734084b96d885eae2c6d525405e70
RacistPumpkin <Standavyhlidal@gmail.com>
2016-02-22 21:16:39 +0100

    Added files via upload

---
+++ appka/build/classes/appka/Hra.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appka;
+
+/**
+ *
+ * @author Stanislav Vyhlídal
+ */
+public class Hra {
+    private String nazev;
+    private double cena;
+
+    public String getNazev() {
+        return nazev;
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public void setNazev(String nazev) {
+        this.nazev = nazev;
+    }
+
+    public void setCena(double cena) {
+        this.cena = cena;
+    }
+    
+    
+    
+}
